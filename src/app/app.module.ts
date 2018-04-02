d95f6e47465575d8214f800263f1bbaf6e38f764
Adam <adam.waniak1@gmail.com>
2018-04-02 13:23:21 +0200

    Provided in memory data database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './hero/heroes/heroes.component';
-import { HeroDetailComponent } from './hero/hero-detail/hero-detail.component';
+import {AppComponent} from './app.component';
+import {HeroesComponent} from './hero/heroes/heroes.component';
+import {HeroDetailComponent} from './hero/hero-detail/hero-detail.component';
 import {HeroService} from "./hero/hero.service";
-import { MessagesComponent } from './messages/messages.component';
-import { MessageService } from './messages/message.service';
-import { AppRoutingModule } from './/app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import {MessagesComponent} from './messages/messages.component';
+import {MessageService} from './messages/message.service';
+import {AppRoutingModule} from './/app-routing.module';
+import {DashboardComponent} from './dashboard/dashboard.component';
+
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './in-memory-data.service';
+import {HttpClientModule} from "@angular/common/http";
 
 
 @NgModule({
@@ -25,6 +29,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {dataEncapsulation: false})
   ],
   providers: [
     HeroService,
@@ -32,4 +39,5 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/hero/hero.service.ts
+++ src/app/hero/hero.service.ts
@@ -1,24 +1,25 @@
 import {Injectable} from '@angular/core';
-import {HEROES} from "./mock-heroes";
 import {Hero} from "./hero";
 import {Observable} from "rxjs/Observable";
-import {of} from "rxjs/observable/of";
 import {MessageService} from "../messages/message.service";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable()
 export class HeroService {
 
-  constructor(private messageService: MessageService) {
+  heroesUrl = 'api/heroes';
+
+  constructor(private messageService: MessageService, private http: HttpClient) {
   }
 
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id = ${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    return this.http.get<Hero>(this.heroesUrl + `/${id}`)
   }
 
 }
